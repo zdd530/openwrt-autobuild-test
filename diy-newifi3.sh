2ff000bded31d954b5434ff316e4a7cfcfc25671
jessehoo89 <66499464+jessehoo89@users.noreply.github.com>
2024-09-17 10:08:06 +0800

    Update diy-newifi3.sh

--- diy-newifi3.sh
+++ diy-newifi3.sh
@@ -3,14 +3,14 @@
 # Clone a sub-directory of a git repository. Probably replaces "svn co" which is being deprecated by GitHub.
 # Usage: git_sparse_clone $repo_url $repo_branch $local_temp_url $sub_directory $target_location
 
-function git_sparse_clone() {
-    git clone --filter=blob:none --no-checkout --depth=1 -b $2 $1 $3 && cd $3
-    git sparse-checkout init --cone
-    git sparse-checkout set $4
-    git checkout
-    mv $4 ../$5
-    cd ../ && rm -rf $3
-}
+# function git_sparse_clone() {
+#     git clone --filter=blob:none --no-checkout --depth=1 -b $2 $1 $3 && cd $3
+#     git sparse-checkout init --cone
+#     git sparse-checkout set $4
+#     git checkout
+#     mv $4 ../$5
+#     cd ../ && rm -rf $3
+# }
 
 # Clean up dependencies
 # find feeds -name Makefile -exec dirname {} \; | grep -wE 'gn|chinadns-ng|dns2socks|dns2tcp|hysteria|ipt2socks|microsocks|naiveproxy|redsocks2|shadowsocks-rust|shadowsocksr-libev|simple-obfs|sing-box|ssocks|tcping|trojan|v2ray-core|v2ray-geodata|v2ray-plugin|v2raya|xray-core|xray-plugin|lua-neturl|luci-app-ssr-plus|kuci-app-mosdns|mosdns' | xargs rm -rf
