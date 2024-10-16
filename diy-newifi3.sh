106644e83151bd731c476b5ba830199d2b8df3e7
jessehoo89 <66499464+jessehoo89@users.noreply.github.com>
2024-10-16 12:58:00 +0800

    Update diy-newifi3.sh

--- diy-newifi3.sh
+++ diy-newifi3.sh
@@ -3,14 +3,14 @@
 # Clone a sub-directory of a git repository. Probably replaces "svn co" which is being deprecated by GitHub.
 # Usage: git_sparse_clone $repo_url $repo_branch $local_temp_url $sub_directory $target_location
 
-# function git_sparse_clone() {
-#     git clone --filter=blob:none --no-checkout --depth=1 -b $2 $1 $3 && cd $3
-#     git sparse-checkout init --cone
-#     git sparse-checkout set $4
-#     git checkout
-#     mv $4 ../$5
-#     cd ../ && rm -rf $3
-# }
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
 
 # Clean up dependencies
 # find feeds -name Makefile -exec dirname {} \; | grep -wE 'gn|chinadns-ng|dns2socks|dns2tcp|hysteria|ipt2socks|microsocks|naiveproxy|redsocks2|shadowsocks-rust|shadowsocksr-libev|simple-obfs|sing-box|ssocks|tcping|trojan|v2ray-core|v2ray-geodata|v2ray-plugin|v2raya|xray-core|xray-plugin|lua-neturl|luci-app-ssr-plus|kuci-app-mosdns|mosdns' | xargs rm -rf
@@ -26,7 +26,7 @@ sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generat
 # git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/mosdns
 # git clone --depth=1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall/ package
+git_sparse_clone main https://github.com/xiaorouji/openwrt-passwall/ package/luci-app-passwall
 
 # Update Go to 1.20 for Xray-core build
 # rm -rf feeds/packages/lang/golang
