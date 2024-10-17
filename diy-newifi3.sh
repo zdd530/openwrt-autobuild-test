7b99a01e6a5fe45f6f4ed579d0481e7cf0ed37ce
jessehoo89 <66499464+jessehoo89@users.noreply.github.com>
2024-10-17 17:12:33 +0800

    Update diy-newifi3.sh

--- diy-newifi3.sh
+++ diy-newifi3.sh
@@ -25,7 +25,7 @@ sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generat
 # git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/mosdns
 # git clone --depth=1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
-＃ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
 
