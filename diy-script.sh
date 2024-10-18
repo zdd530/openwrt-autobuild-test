4fe79e5b7a74ccf0862d4b1225799d8aafd621f7
jessehoo89 <66499464+jessehoo89@users.noreply.github.com>
2024-10-18 11:12:28 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -43,7 +43,7 @@ git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/
 
 # 科学上网插件
 # git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
-# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
 # git_sparse_clone master https://github.com/vernesong/OpenClash package/luci-app-openclash
