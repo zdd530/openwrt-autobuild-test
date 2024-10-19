ef67e2c27d2fc47474335f407a44f0d951c3fd43
jessehoo89 <66499464+jessehoo89@users.noreply.github.com>
2024-10-19 21:52:14 +0800

    Update diy-newifi3.sh

--- diy-newifi3.sh
+++ diy-newifi3.sh
@@ -3,17 +3,17 @@
 # Clone a sub-directory of a git repository. Probably replaces "svn co" which is being deprecated by GitHub.
 # Usage: git_sparse_clone $repo_url $repo_branch $local_temp_url $sub_directory $target_location
 
-rm -rf feeds/packages/net/mosdns
-rm -rf feeds/luci/applications/luci-app-mosdns
+# rm -rf feeds/packages/net/mosdns
+# rm -rf feeds/luci/applications/luci-app-mosdns
 
-function git_sparse_clone() {
-  branch="$1" repourl="$2" && shift 2
-  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
-  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
-  cd $repodir && git sparse-checkout set $@
-  mv -f $@ ../package
-  cd .. && rm -rf $repodir
-}
+# function git_sparse_clone() {
+#   branch="$1" repourl="$2" && shift 2
+#   git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+#   repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+#   cd $repodir && git sparse-checkout set $@
+#   mv -f $@ ../package
+#   cd .. && rm -rf $repodir
+# }
 
 # Clean up dependencies
 # find feeds -name Makefile -exec dirname {} \; | grep -wE 'gn|chinadns-ng|dns2socks|dns2tcp|hysteria|ipt2socks|microsocks|naiveproxy|redsocks2|shadowsocks-rust|shadowsocksr-libev|simple-obfs|sing-box|ssocks|tcping|trojan|v2ray-core|v2ray-geodata|v2ray-plugin|v2raya|xray-core|xray-plugin|lua-neturl|luci-app-ssr-plus|kuci-app-mosdns|mosdns' | xargs rm -rf
@@ -28,8 +28,8 @@ sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generat
 # git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/mosdns
 # git clone --depth=1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
-git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
-git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
+# git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
+# git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
@@ -38,5 +38,5 @@ git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app
 # rm -rf feeds/packages/lang/golang
 # git_sparse_clone https://github.com/openwrt/packages master packages-upstream lang/golang feeds/packages/lang/golang
 
-./scripts/feeds update -a
-./scripts/feeds install -a
+# ./scripts/feeds update -a
+# ./scripts/feeds install -a
