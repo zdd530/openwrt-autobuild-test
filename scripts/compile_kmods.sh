714a280047f72ab5b8d48d055a8d3dd0a62e8530
jessehoo89 <66499464+jessehoo89@users.noreply.github.com>
2024-10-20 17:52:35 +0800

    Update compile_kmods.sh

--- scripts/compile_kmods.sh
+++ scripts/compile_kmods.sh
@@ -5,19 +5,10 @@
 # 编译内核
 make -j17 target/linux/{clean,compile} V=s LINUX_VERMAGIC:=00bdd56aed0992cf77d7e3c1c49495e7
 
-# 定义内核模块目录
-KERNEL_DIR="package/kernel/linux"
-
-# 检查内核模块目录是否存在
-if [ ! -d "$KERNEL_DIR" ]; then
-  echo "Error: Directory $KERNEL_DIR does not exist."
-  exit 1
-fi
-
 # 编译内核模块
 echo "Compiling kernel modules..."
-make -j$(nproc) -k LINUX_VERMAGIC:=00bdd56aed0992cf77d7e3c1c49495e7 "$KERNEL_DIR"/compile V=s
+make V=s LINUX_VERMAGIC:=00bdd56aed0992cf77d7e3c1c49495e7 ./package/kernel/linux/{clean,compile}
 
-make "$KERNEL_DIR"/index
+make bin/targets/ramips/mt7621/packages/index
 
 echo "All modules that could be compiled have been compiled."
