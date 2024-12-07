089b944f92df6ec1590feacafcec7b6d00968665
jessehoo89 <66499464+jessehoo89@users.noreply.github.com>
2024-12-07 19:20:32 +0800

    Update diy-jdc_ax1800_pro-script.sh

--- diy-jdc_ax1800_pro-script.sh
+++ diy-jdc_ax1800_pro-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd openwrt
-echo "OPENWRT_PATH=$PWD" >> $GITHUB_ENV
+# cd openwrt
+# echo "OPENWRT_PATH=$PWD" >> $GITHUB_ENV
 
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
@@ -37,7 +37,7 @@ function git_sparse_clone() {
 # git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 # git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush package/luci-app-serverchan
 # git clone --depth=1 https://github.com/ilxp/luci-app-ikoolproxy package/luci-app-ikoolproxy
-git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
+# git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
 # git clone --depth=1 https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 # git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
 # git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebrowser luci-app-ssr-mudb-server
@@ -102,9 +102,9 @@ git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/
 # sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
 
 # 修改版本为编译日期
-date_version=$(date +"%y.%m.%d")
-orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
-sed -i "s/${orig_version}/R${date_version} by Haiibo/g" package/lean/default-settings/files/zzz-default-settings
+# date_version=$(date +"%y.%m.%d")
+# orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+# sed -i "s/${orig_version}/R${date_version} by Haiibo/g" package/lean/default-settings/files/zzz-default-settings
 
 # 修复 hostapd 报错
 # cp -f $GITHUB_WORKSPACE/scripts/011-fix-mbo-modules-build.patch package/network/services/hostapd/patches/011-fix-mbo-modules-build.patch
@@ -135,7 +135,3 @@ if ! grep -q "small-package" "$OPENWRT_PATH/feeds.conf.default"; then
     # 添加 small-package 源
     echo "src-git small8 https://github.com/kenzok8/small-package" >>"$OPENWRT_PATH/feeds.conf.default"
 fi
-
-./scripts/feeds update -a
-./scripts/feeds install -a
-
