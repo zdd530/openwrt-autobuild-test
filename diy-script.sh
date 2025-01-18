64e5f3241004ffb7a29457d184b860054727166a
jessehoo89 <66499464+jessehoo89@users.noreply.github.com>
2025-01-18 22:37:50 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -118,16 +118,16 @@ sed -i "s/${orig_version}/R${date_version} by Haiibo/g" package/lean/default-set
 #     sed -i '$a src-git small8 https://github.com/kenzok8/small-package' $OPENWRT_PATH/feeds.conf.default
 # fi
 
-fix_miniupmpd() {
-    local PKG_HASH=$(awk -F"=" '/^PKG_HASH:/ {print $2}' ./feeds/packages/net/miniupnpd/Makefile)
-    if [[ $PKG_HASH == "fbdd5501039730f04a8420ea2f8f54b7df63f9f04cde2dc67fa7371e80477bbe" ]]; then
-        if [[ -f $BASE_PATH/patches/400-fix_nft_miniupnp.patch ]]; then
-            if [[ ! -d ./feeds/packages/net/miniupnpd/patches ]]; then
-                mkdir -p ./feeds/packages/net/miniupnpd/patches
-            fi
-            \cp -f $BASE_PATH/patches/400-fix_nft_miniupnp.patch ./feeds/packages/net/miniupnpd/patches/
-        fi
-    fi
-}
+# fix_miniupmpd() {
+#     local PKG_HASH=$(awk -F"=" '/^PKG_HASH:/ {print $2}' ./feeds/packages/net/miniupnpd/Makefile)
+#     if [[ $PKG_HASH == "fbdd5501039730f04a8420ea2f8f54b7df63f9f04cde2dc67fa7371e80477bbe" ]]; then
+#         if [[ -f $BASE_PATH/patches/400-fix_nft_miniupnp.patch ]]; then
+#             if [[ ! -d ./feeds/packages/net/miniupnpd/patches ]]; then
+#                 mkdir -p ./feeds/packages/net/miniupnpd/patches
+#             fi
+#             \cp -f $BASE_PATH/patches/400-fix_nft_miniupnp.patch ./feeds/packages/net/miniupnpd/patches/
+#         fi
+#     fi
+# }
 
-fix_miniupmpd
+# fix_miniupmpd
