7be0f96e5ec84c20f4ae6f3d2dd52be724c9a6e2
jessehoo89 <66499464+jessehoo89@users.noreply.github.com>
2025-01-18 22:17:49 +0800

    Create fix_upnpd.sh

---
+++ fix_upnpd.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# 修复24.10版本upnpd
+fix_miniupnpd() {
+    local PKG_HASH=$(awk -F"=" '/^PKG_HASH:/ {print $2}' ./feeds/packages/net/miniupnpd/Makefile)
+    if [[ $PKG_HASH == "fbdd5501039730f04a8420ea2f8f54b7df63f9f04cde2dc67fa7371e80477bbe" ]]; then
+        if [[ -f $BASE_PATH/patches/400-fix_nft_miniupnp.patch ]]; then
+            if [[ ! -d ./feeds/packages/net/miniupnpd/patches ]]; then
+                mkdir -p ./feeds/packages/net/miniupnpd/patches
+            fi
+            \cp -f $BASE_PATH/patches/400-fix_nft_miniupnp.patch ./feeds/packages/net/miniupnpd/patches/
+        fi
+    fi
+}
+
+fix_miniupnpd
