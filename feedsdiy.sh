60c6d159ff171cc5794a2631863775209ec5bc55
jessehoo89 <66499464+jessehoo89@users.noreply.github.com>
2025-01-18 21:22:04 +0800

    Update feedsdiy.sh

--- feedsdiy.sh
+++ feedsdiy.sh
@@ -81,18 +81,18 @@ install_small8() {
     v2ray-core shadowsocks-rust xray-core
 }
 
-fix_miniupmpd() {
-    local PKG_HASH=$(awk -F"=" '/^PKG_HASH:/ {print $2}' ./feeds/packages/net/miniupnpd/Makefile)
-    if [[ $PKG_HASH == "fbdd5501039730f04a8420ea2f8f54b7df63f9f04cde2dc67fa7371e80477bbe" ]]; then
-        if [[ -f $BASE_PATH/patches/400-fix_nft_miniupnp.patch ]]; then
-            if [[ ! -d ./feeds/packages/net/miniupnpd/patches ]]; then
-                mkdir -p ./feeds/packages/net/miniupnpd/patches
-            fi
-            \cp -f $BASE_PATH/patches/400-fix_nft_miniupnp.patch ./feeds/packages/net/miniupnpd/patches/
-        fi
-    fi
-}
+# fix_miniupmpd() {
+#     local PKG_HASH=$(awk -F"=" '/^PKG_HASH:/ {print $2}' ./feeds/packages/net/miniupnpd/Makefile)
+#     if [[ $PKG_HASH == "fbdd5501039730f04a8420ea2f8f54b7df63f9f04cde2dc67fa7371e80477bbe" ]]; then
+#         if [[ -f $BASE_PATH/patches/400-fix_nft_miniupnp.patch ]]; then
+#             if [[ ! -d ./feeds/packages/net/miniupnpd/patches ]]; then
+#                 mkdir -p ./feeds/packages/net/miniupnpd/patches
+#             fi
+#             \cp -f $BASE_PATH/patches/400-fix_nft_miniupnp.patch ./feeds/packages/net/miniupnpd/patches/
+#         fi
+#     fi
+# }
 
 # remove_unwanted_packages
 install_small8
-fix_miniupmpd
+# fix_miniupmpd
