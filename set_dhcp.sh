b2d81fb58d13099451fa245a60c474f09b4db5f1
jessehoo89 <66499464+jessehoo89@users.noreply.github.com>
2025-01-24 16:42:42 +0800

    Update set_dhcp.sh

--- set_dhcp.sh
+++ set_dhcp.sh
@@ -2,7 +2,7 @@
 
 # 设置 LAN 口的 DHCP 为关闭状态
 set_dhcp_off() {
-    sed -i "/config dhcp lan/,/^[[:space:]]*$/ {/^[[:space:]]*$/i\	option ignore	1}" $GITHUB_WORKSPACE/openwrt/package/network/services/dnsmasq/files/dhcp.conf
+    sed -i "/config dhcp lan/,/^[[:space:]]*$/ {/^[[:space:]]*$/i\	option ignore	1}" ./package/network/services/dnsmasq/files/dhcp.conf
     echo "LAN 口的 DHCP 服务已设置为默认关闭。"
 }
 
